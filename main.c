906e57c962e76529c7a43369ac1333b233067f70
Adreeezz <127537483+Adreeezz@users.noreply.github.com>
2023-11-26 07:22:51 +0000

    Algunos prototipos y estructura para los nodos

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ Estructuras de Datos II
 Fecha de publicación: Martes 21 de noviembre de 2023
 Fecha de entrega: Martes 28 de noviembre de 2023
 
-Nombre del alumno: ___________________________________________
+Nombre del alumno: Lara López Fryda Renata
 
 Nombre de usuario en omegaUp: ________________________________
 
@@ -19,10 +19,25 @@ Nombre de usuario en omegaUp: ________________________________
 // IMPORTANT: Comment the next line before submitting to omegaUp
 #define DEBUG
 
+typedef struct node{
+    int key;
+    struct node *left;
+    struct node *right;
+    int fe;
+    int height;
+}node_t;
+
 void insert_node();
 void erase_node();
 void print_height();
 void print_by_level();
+node_t* create_node( int key);
+int height(node_t *node);
+void act_height( node_t *node);
+void balance_factor( node_t *node);
+void right_rotation(node_t **root);
+void left_rotation(node_t **root);
+
 
 int main(void)  {
     // IMPORTANT: Comment the next line before submitting to omegaUp

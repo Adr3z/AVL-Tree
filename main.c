62b7faa623ac0547051c59f4a6ed2f438e8f2fc2
Adreeezz <127537483+Adreeezz@users.noreply.github.com>
2023-11-26 08:18:52 +0000

    Funciones para crear nodos, insertar nodos y liberar el árbol

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ Nombre de usuario en omegaUp: ________________________________
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 // IMPORTANT: Comment the next line before submitting to omegaUp
@@ -27,23 +28,24 @@ typedef struct node{
     int height;
 }node_t;
 
-void insert_node();
+void insert_node( node_t **root);
 void erase_node();
 void print_height();
 void print_by_level();
 node_t* create_node( int key);
 int height(node_t *node);
-void act_height( node_t *node);
-void balance_factor( node_t *node);
+int balance_factor( node_t *node);
 void right_rotation(node_t **root);
 void left_rotation(node_t **root);
-
+void aux_insert(node_t **root, int value);
+void free_tree(node_t* root);
 
 int main(void)  {
     // IMPORTANT: Comment the next line before submitting to omegaUp
     freopen("input.txt", "rt", stdin);
     // freopen("output.txt", "rt", stdout);
 
+    node_t *root = NULL;
     int number_of_operations;
     scanf("%d", &number_of_operations);
     while (number_of_operations--) {
@@ -51,7 +53,7 @@ int main(void)  {
         scanf("\n%c", &operation);
         switch (toupper(operation)) {
             case 'I':
-                insert_node();
+                insert_node(&root);
             break;
             case 'E':   
                 erase_node();
@@ -64,10 +66,11 @@ int main(void)  {
             break;
         }
     }
+    free_tree(root);
     return 0;
 }
 
-void insert_node()
+void insert_node( node_t **root)
 {
     int value;
     scanf("%d", &value);
@@ -75,6 +78,7 @@ void insert_node()
     printf("Inserting node with value %d\n", value);
 #endif
     // TODO: insert node with the specified value
+    aux_insert(&(*root), value);
 }
 
 void erase_node()
@@ -101,4 +105,110 @@ void print_by_level()
     printf("Printing the tree's content level by level\n");
 #endif
     // TODO: print the tree's content level by level
+}
+
+node_t* create_node( int key)
+{
+    node_t *new = (node_t*)malloc(sizeof(node_t));
+    new->key = key;
+    new->left = new->right = NULL;
+    new->fe = new->height = 0;
+    return new;
+}
+
+void aux_insert(node_t **root, int value)
+{
+    if (*root == NULL) {
+        *root = create_node(value);
+        return;
+    }
+
+    if (value < (*root)->key) {
+        aux_insert(&((*root)->left), value);
+    } else if (value > (*root)->key) {
+        aux_insert(&((*root)->right), value);
+    } else {
+        return; // No permitir claves duplicadas
+    }
+
+    // Actualizar la altura del nodo actual
+    (*root)->height = height(*root);
+
+    // Calcular el factor de equilibrio del nodo actual
+    int fe = balance_factor(*root);
+
+    // Realizar rotaciones según los casos de desequilibrio
+    // (izquierda-izquierda, izquierda-derecha, derecha-derecha, derecha-izquierda)
+    if (fe > 1 && value > (*root)->right->key) {
+        left_rotation(root);
+    }
+
+    if (fe < -1 && value < (*root)->left->key) {
+        right_rotation(root);
+    }
+
+    if (fe > 1 && value < (*root)->right->key) {
+        right_rotation(&((*root)->right));
+        left_rotation(root);
+    }
+
+    if (fe < -1 && value > (*root)->left->key) {
+        left_rotation(&((*root)->left));
+        right_rotation(root);
+    }
+}
+int height(node_t *node)
+{
+    if(node == NULL){
+        return -1;
+    }
+    int left = height(node->left);
+    int right = height(node->right);
+    return 1 + (left > right ? left : right);
+}
+
+int balance_factor( node_t *node)
+{
+    return (node->right == NULL ? -1 : node->right->height) - (node->left == NULL ? -1 : node->left->height);
+}
+
+void right_rotation(node_t **root)
+{
+    node_t *y = *root;
+    node_t *x = y->left;
+    node_t *t2 = x->right;
+
+    x->right = y;
+    y->left = t2;
+    *root = x;
+
+    x->height = height(x);
+    y->height = height(y);
+    x->fe = balance_factor(x);
+    y->fe = balance_factor(y);
+}
+
+void left_rotation(node_t **root)
+{
+    node_t *x = *root;
+    node_t *y =x->right;
+    node_t *t2 = y->left;
+
+    y->left = x;
+    x->right = t2;
+    *root = y;
+
+    y->height = height(y);
+    x->height = height(x);
+    y->fe = balance_factor(y);
+    x->fe = balance_factor(x);
+}
+
+void free_tree(node_t* root) 
+{
+    if (root != NULL) {
+        free_tree(root->left);
+        free_tree(root->right);
+        free(root);
+    }
 }
